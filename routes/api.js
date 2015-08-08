393ec51194a15b3a64aaf0831ba5f31faa447552
Nick Randall <nick.randall@domo.com>
2015-08-07 22:19:33 -0600

    fix(api): allowing CORS

--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,11 @@ var cookieRequest = request.defaults({jar: j});
 
 /* GET users listing. */
 router
+  .all('/', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    next();
+  })
   .get('/', function(req, res, next) {
     res.render('api', {});
   })
