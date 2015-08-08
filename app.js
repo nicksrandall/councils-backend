0638070ff55f8a58a633944366cbbc1b648ee71f
Nick Randall <nick.randall@domo.com>
2015-08-07 22:24:52 -0600

    fixing cors again

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // enable CORS
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
